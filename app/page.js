13ddd93418958fdf31f1dd6be5688f7a0bd35f08
RITIK KOHLI <ritikkohli73@gmail.com>
2023-10-03 02:53:51 +0530

    responsive

--- app/page.js
+++ app/page.js
@@ -13,20 +13,43 @@ export default function Home(){
             return alert('please provide url');
         }
         if(!urlRegex.test(url)){
+            seturl('');
             return alert('invalid url');
         }
-        setqrUrl(`http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=300x300`);
+        setqrUrl(`http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=300x300&mrgin=20`);
         seturl('');
     }
 
+    function downloadQR(e,qrUrl){
+        e.preventDefault();
+        if(qrUrl == 'https://i.ibb.co/31BW7NR/temp-Image.png') return
+        const filename = Math.random().toString(36).substring(2,9)+'.png';
+        
+        fetch(qrUrl)
+        .then((response) => response.blob())
+        .then((blob) => {
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            document.body.appendChild(a);
+
+            const blobUrl = window.URL.createObjectURL(blob);
+            a.href = blobUrl;
+            a.download = filename;
+            a.click();
+
+            window.URL.revokeObjectURL(blobUrl);
+        });
+    }
     return (
-    <div>
+    <div id="main">
         <div id="top-panel">
-            <input type="url" placeholder="enter url" onChange={e=>seturl(e.target.value)} id="url-box"/>
-            <button id="btn" onClick={get}>Genrate</button>
-        </div>
-        <div id="main">
-            <img src={qrUrl} alt="QR code"/>
+            <input type="url" value={url} placeholder="enter url" onChange={e=>seturl(e.target.value)} id="url-box"/>
+            <button className="btn" onClick={get}>Generate</button>
+            <div id="image-container">
+                <img id="QRimage" src={qrUrl} alt="QR code"/>
+            </div>
+            <button onClick={(e)=>downloadQR(e,qrUrl)} className="btn"><i className="ri-download-line"/></button>
+            <a id="git" target="_blank" href="https://github.com/ritikkohli"><i class="ri-github-line"/></a>
         </div>
     </div>
     )
